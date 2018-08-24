72bd47d8bd26427bb22bfb81d2d2c164708a0e06
christiansiegel <christiansiegel@users.noreply.github.com>
2018-08-24 19:08:28 +0200

    add paginator data and props tests

--- src/components/paginator.spec.js
+++ src/components/paginator.spec.js
@@ -1,8 +1,23 @@
 
 import Paginator from './paginator.vue'
 
-describe('Paginator', function () {
-  it('is defined', function () {
-    expect(typeof(Paginator)).toEqual('object')
+describe('Paginator', () =>  {
+  it('is defined', () =>  {
+    expect(typeof(Paginator)).toBe('object')
+  })
+
+  it('sets the correct default data', () => {
+    expect(typeof Paginator.data).toBe('function')
+    const defaultData = Paginator.data()
+    expect(defaultData.chunkSize).toBe(10)
+  })
+
+  it('has the correct props', () => {
+    expect(typeof Paginator.props).toBe('object')
+    const props = Paginator.props
+    expect(props).toEqual(jasmine.objectContaining({
+      total: Number,
+      current: Number
+    }));
   })
 })
