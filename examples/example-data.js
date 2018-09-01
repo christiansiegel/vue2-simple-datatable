52bb47d7b4201abb8dfc445d270b39c6aeee50d2
christiansiegel <christiansiegel@users.noreply.github.com>
2018-09-01 11:33:11 +0200

    added basic example

---
+++ examples/example-data.js
@@ -0,0 +1,752 @@
+var persons = [
+  {
+    first: 'Corina',
+    last: 'Bouchier',
+    age: 60
+  },
+  {
+    first: 'Lyndsey',
+    last: 'Bollay',
+    age: 70
+  },
+  {
+    first: 'Evanne',
+    last: 'Weinberg',
+    age: 45
+  },
+  {
+    first: 'Maren',
+    last: 'Nikolai',
+    age: 26
+  },
+  {
+    first: 'Eddie',
+    last: 'Norvun',
+    age: 33
+  },
+  {
+    first: 'Staci',
+    last: 'Putnem',
+    age: 53
+  },
+  {
+    first: 'Pollyanna',
+    last: 'Yona',
+    age: 79
+  },
+  {
+    first: 'Aretha',
+    last: 'Leff',
+    age: 38
+  },
+  {
+    first: 'Lynnette',
+    last: 'Debera',
+    age: 47
+  },
+  {
+    first: 'Mathilda',
+    last: 'Min',
+    age: 36
+  },
+  {
+    first: 'Coralie',
+    last: 'Strohbehn',
+    age: 85
+  },
+  {
+    first: 'Merrili',
+    last: 'Trueman',
+    age: 40
+  },
+  {
+    first: 'Caril',
+    last: 'Doretta',
+    age: 3
+  },
+  {
+    first: 'Joannes',
+    last: 'Krystle',
+    age: 81
+  },
+  {
+    first: 'Suzann',
+    last: 'Julita',
+    age: 59
+  },
+  {
+    first: 'Nert',
+    last: 'Eupheemia',
+    age: 47
+  },
+  {
+    first: 'Gwenora',
+    last: 'Brufsky',
+    age: 75
+  },
+  {
+    first: 'Doll',
+    last: 'Lula',
+    age: 56
+  },
+  {
+    first: 'Bertie',
+    last: 'Treulich',
+    age: 11
+  },
+  {
+    first: 'Kathe',
+    last: 'Annetta',
+    age: 90
+  },
+  {
+    first: 'Toinette',
+    last: 'Pry',
+    age: 21
+  },
+  {
+    first: 'Brittany',
+    last: 'Elsi',
+    age: 92
+  },
+  {
+    first: 'Nora',
+    last: 'Volin',
+    age: 65
+  },
+  {
+    first: 'Sonnie',
+    last: 'Newsom',
+    age: 53
+  },
+  {
+    first: 'Pennie',
+    last: 'Patrich',
+    age: 67
+  },
+  {
+    first: 'Emeline',
+    last: 'Allis',
+    age: 81
+  },
+  {
+    first: 'Martha',
+    last: 'Litch',
+    age: 67
+  },
+  {
+    first: 'Evvy',
+    last: 'Jerry',
+    age: 41
+  },
+  {
+    first: 'Felita',
+    last: 'Lauryn',
+    age: 44
+  },
+  {
+    first: 'Delly',
+    last: 'Felton',
+    age: 36
+  },
+  {
+    first: 'Mireielle',
+    last: 'Alisun',
+    age: 18
+  },
+  {
+    first: 'Colette',
+    last: 'Ahern',
+    age: 17
+  },
+  {
+    first: 'Imojean',
+    last: 'Declan',
+    age: 99
+  },
+  {
+    first: 'Astra',
+    last: 'Pachston',
+    age: 21
+  },
+  {
+    first: 'Essie',
+    last: 'Higley',
+    age: 1
+  },
+  {
+    first: 'Sophie',
+    last: 'Zailer',
+    age: 50
+  },
+  {
+    first: 'Heddi',
+    last: 'Vaughn',
+    age: 98
+  },
+  {
+    first: 'Rebe',
+    last: 'Fiedling',
+    age: 81
+  },
+  {
+    first: 'Roda',
+    last: 'Gettings',
+    age: 38
+  },
+  {
+    first: 'Marnia',
+    last: 'Rosalind',
+    age: 11
+  },
+  {
+    first: 'Martica',
+    last: 'Kokaras',
+    age: 28
+  },
+  {
+    first: 'Glennis',
+    last: 'Warthman',
+    age: 58
+  },
+  {
+    first: 'Amye',
+    last: 'Merline',
+    age: 63
+  },
+  {
+    first: 'Jo',
+    last: 'Karas',
+    age: 56
+  },
+  {
+    first: 'Demetra',
+    last: 'Jessamyn',
+    age: 60
+  },
+  {
+    first: 'Sibel',
+    last: 'Swayder',
+    age: 45
+  },
+  {
+    first: 'Edy',
+    last: 'Shanon',
+    age: 47
+  },
+  {
+    first: 'Cody',
+    last: 'Laurita',
+    age: 56
+  },
+  {
+    first: 'Sib',
+    last: 'Tadeo',
+    age: 24
+  },
+  {
+    first: 'Kayle',
+    last: 'Dollar',
+    age: 34
+  },
+  {
+    first: 'Maddie',
+    last: 'Evetta',
+    age: 57
+  },
+  {
+    first: 'Kelcey',
+    last: 'Thompson',
+    age: 2
+  },
+  {
+    first: 'Veronique',
+    last: 'Vitale',
+    age: 77
+  },
+  {
+    first: 'Celestine',
+    last: 'Quartis',
+    age: 5
+  },
+  {
+    first: 'Jeralee',
+    last: 'Blinni',
+    age: 18
+  },
+  {
+    first: 'Norah',
+    last: 'Alpert',
+    age: 93
+  },
+  {
+    first: 'Elsinore',
+    last: 'Mitzi',
+    age: 56
+  },
+  {
+    first: 'Shelia',
+    last: 'Ursulina',
+    age: 74
+  },
+  {
+    first: 'Sandie',
+    last: 'Saylor',
+    age: 56
+  },
+  {
+    first: 'Lelah',
+    last: 'Sprague',
+    age: 61
+  },
+  {
+    first: 'Essa',
+    last: 'Longerich',
+    age: 63
+  },
+  {
+    first: 'Brunhilde',
+    last: 'White',
+    age: 17
+  },
+  {
+    first: 'Jeralee',
+    last: 'Kinson',
+    age: 81
+  },
+  {
+    first: 'Beret',
+    last: 'Halli',
+    age: 25
+  },
+  {
+    first: 'Gracie',
+    last: 'Anatole',
+    age: 32
+  },
+  {
+    first: 'Kerianne',
+    last: 'Marcile',
+    age: 80
+  },
+  {
+    first: 'Amelia',
+    last: 'Othelia',
+    age: 48
+  },
+  {
+    first: 'Vale',
+    last: 'Tolley',
+    age: 62
+  },
+  {
+    first: 'Latrina',
+    last: 'Radbun',
+    age: 76
+  },
+  {
+    first: 'Tarrah',
+    last: 'Flavius',
+    age: 94
+  },
+  {
+    first: 'Lil',
+    last: 'Changaris',
+    age: 3
+  },
+  {
+    first: 'Ema',
+    last: 'Gavrah',
+    age: 65
+  },
+  {
+    first: 'Risa',
+    last: 'Gelya',
+    age: 80
+  },
+  {
+    first: 'Yolanda',
+    last: 'Howlond',
+    age: 8
+  },
+  {
+    first: 'Babs',
+    last: 'Petersen',
+    age: 93
+  },
+  {
+    first: 'Abbie',
+    last: 'Kathleen',
+    age: 69
+  },
+  {
+    first: 'Franny',
+    last: 'Brower',
+    age: 57
+  },
+  {
+    first: 'Marian',
+    last: 'Macey',
+    age: 19
+  },
+  {
+    first: 'Sallie',
+    last: 'Rabassa',
+    age: 32
+  },
+  {
+    first: 'Indira',
+    last: 'Mommy',
+    age: 28
+  },
+  {
+    first: 'Lilias',
+    last: 'Hadria',
+    age: 57
+  },
+  {
+    first: 'Evanne',
+    last: 'Gleich',
+    age: 74
+  },
+  {
+    first: 'Merola',
+    last: 'Dyson',
+    age: 43
+  },
+  {
+    first: 'Debera',
+    last: 'Kenji',
+    age: 83
+  },
+  {
+    first: 'Almira',
+    last: 'Hazem',
+    age: 86
+  },
+  {
+    first: 'Nannie',
+    last: 'Weir',
+    age: 78
+  },
+  {
+    first: 'Kirsteni',
+    last: 'Clere',
+    age: 65
+  },
+  {
+    first: 'Carey',
+    last: 'Amelita',
+    age: 64
+  },
+  {
+    first: 'Marillin',
+    last: 'Lilyan',
+    age: 91
+  },
+  {
+    first: 'Lianne',
+    last: 'Yokoyama',
+    age: 78
+  },
+  {
+    first: 'Rozanne',
+    last: 'Gerrit',
+    age: 30
+  },
+  {
+    first: 'Melesa',
+    last: 'Rumery',
+    age: 80
+  },
+  {
+    first: 'Ambur',
+    last: 'Mattox',
+    age: 12
+  },
+  {
+    first: 'Annabella',
+    last: 'Marley',
+    age: 83
+  },
+  {
+    first: 'Meris',
+    last: 'Alexina',
+    age: 22
+  },
+  {
+    first: 'Shay',
+    last: 'Treva',
+    age: 9
+  },
+  {
+    first: 'Lilia',
+    last: 'Pincas',
+    age: 6
+  },
+  {
+    first: 'Marylee',
+    last: 'Bhatt',
+    age: 99
+  },
+  {
+    first: 'Goldi',
+    last: 'Adrien',
+    age: 67
+  },
+  {
+    first: 'Leah',
+    last: 'Madel',
+    age: 86
+  },
+  {
+    first: 'Dianna',
+    last: 'Neumeyer',
+    age: 30
+  },
+  {
+    first: 'Trudi',
+    last: 'Keverne',
+    age: 36
+  },
+  {
+    first: 'Lydia',
+    last: 'Hazen',
+    age: 69
+  },
+  {
+    first: 'Raven',
+    last: 'Dash',
+    age: 16
+  },
+  {
+    first: 'Ethelind',
+    last: 'Gracye',
+    age: 54
+  },
+  {
+    first: 'Devina',
+    last: 'Khano',
+    age: 94
+  },
+  {
+    first: 'Elisha',
+    last: 'Herriott',
+    age: 67
+  },
+  {
+    first: 'Kaylil',
+    last: 'Ripp',
+    age: 78
+  },
+  {
+    first: 'Tiffie',
+    last: 'Mayfield',
+    age: 74
+  },
+  {
+    first: 'Gillan',
+    last: 'Ashok',
+    age: 48
+  },
+  {
+    first: 'Mab',
+    last: 'Parrish',
+    age: 58
+  },
+  {
+    first: 'Debor',
+    last: 'Alane',
+    age: 20
+  },
+  {
+    first: 'Darda',
+    last: 'Jeffery',
+    age: 44
+  },
+  {
+    first: 'Regina',
+    last: 'Harrell',
+    age: 56
+  },
+  {
+    first: 'Carissa',
+    last: 'Simeon',
+    age: 68
+  },
+  {
+    first: 'Gwenette',
+    last: 'Danit',
+    age: 75
+  },
+  {
+    first: 'Allys',
+    last: 'Basir',
+    age: 94
+  },
+  {
+    first: 'Codee',
+    last: 'Forlini',
+    age: 42
+  },
+  {
+    first: 'Evangelina',
+    last: 'Standish',
+    age: 7
+  },
+  {
+    first: 'Silvie',
+    last: 'Bank',
+    age: 69
+  },
+  {
+    first: 'Maitilde',
+    last: 'Goto',
+    age: 75
+  },
+  {
+    first: 'Glenna',
+    last: 'Harolda',
+    age: 39
+  },
+  {
+    first: 'Jessalin',
+    last: 'Fenton',
+    age: 11
+  },
+  {
+    first: 'Vivianna',
+    last: 'Anderegg',
+    age: 20
+  },
+  {
+    first: 'Berta',
+    last: 'Oberon',
+    age: 37
+  },
+  {
+    first: 'Shannah',
+    last: 'McGean',
+    age: 89
+  },
+  {
+    first: 'Karilynn',
+    last: 'Lochner',
+    age: 89
+  },
+  {
+    first: 'Kerianne',
+    last: 'Christenson',
+    age: 35
+  },
+  {
+    first: 'Shoshana',
+    last: 'Sankaran',
+    age: 18
+  },
+  {
+    first: 'Clarissa',
+    last: 'Isia',
+    age: 22
+  },
+  {
+    first: 'Mariska',
+    last: 'Tati',
+    age: 49
+  },
+  {
+    first: 'Jena',
+    last: 'Varden',
+    age: 4
+  },
+  {
+    first: 'Estrellita',
+    last: 'Lucinda',
+    age: 99
+  },
+  {
+    first: 'Annalise',
+    last: 'Urian',
+    age: 52
+  },
+  {
+    first: 'Doris',
+    last: 'Dempstor',
+    age: 72
+  },
+  {
+    first: 'Catharina',
+    last: 'Laurance',
+    age: 59
+  },
+  {
+    first: 'Beitris',
+    last: 'Tut',
+    age: 39
+  },
+  {
+    first: 'Lila',
+    last: 'Melosa',
+    age: 9
+  },
+  {
+    first: 'Tim',
+    last: 'Zorana',
+    age: 62
+  },
+  {
+    first: 'Florencia',
+    last: 'Zucker',
+    age: 82
+  },
+  {
+    first: 'Shoshana',
+    last: 'Ira',
+    age: 91
+  },
+  {
+    first: 'Roxanne',
+    last: 'Granlund',
+    age: 68
+  },
+  {
+    first: 'Jeniffer',
+    last: 'Juline',
+    age: 9
+  },
+  {
+    first: 'Arleta',
+    last: 'Saimon',
+    age: 85
+  },
+  {
+    first: 'Dyane',
+    last: 'Sjoberg',
+    age: 52
+  },
+  {
+    first: 'Lianne',
+    last: 'Wilmar',
+    age: 51
+  },
+  {
+    first: 'Herminia',
+    last: 'Willumsen',
+    age: 75
+  },
+  {
+    first: 'Albertina',
+    last: 'Barncard',
+    age: 31
+  },
+  {
+    first: 'Shari',
+    last: 'Farah',
+    age: 30
+  },
+  {
+    first: 'Bibbye',
+    last: 'Boy',
+    age: 89
+  }
+]
