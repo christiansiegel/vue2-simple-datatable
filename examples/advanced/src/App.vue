40a51a1bb5d7ec0ddd4427ddcb1bfea8a3589676
christiansiegel <christiansiegel@users.noreply.github.com>
2018-09-01 13:10:41 +0200

    added github links

--- examples/advanced/src/App.vue
+++ examples/advanced/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <h1>Advanced Example</h1>
+    <h1>
+      <a class="text-dark" href="https://github.com/christiansiegel/vue2-simple-datatable">
+        vue2-simple-datatable <i class="fab fa-github"></i>
+      </a>
+    </h1>
+    <h2>Advanced Example</h2>
+
     <vue2-simple-datatable :data="data" :columns="columns" />
   </div>
 </template>
